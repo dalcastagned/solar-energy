58e9bd0af56f52090e9d117cc8f4d32a96f4d30a
Daniel Dalcastagne <dalcastagned@gmail.com>
2022-01-23 09:00:37 -0300

    added routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import HomePage from "./pages/HomePage";
 import GlobalStyle, { Container } from "./styles/globalStyles";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from './pages/HomePage'
 
 function App() {
   return (
     <Container>
-      <GlobalStyle />
-      <HomePage/>
+      <Router>
+        <GlobalStyle />
+        <Routes>
+          <Route path="/" element={<HomePage/>} />
+          <Route path="/dashboard" element={<HomePage/>} />
+          <Route path="/unidades" element={<HomePage/>} />
+          <Route path="/unidades/cadastro" element={<HomePage/>} />
+          <Route path="/unidades/edicao/:id" element={<HomePage/>} />
+          <Route path="/geracao/cadastro" element={<HomePage/>} />
+        </Routes>
+      </Router>
     </Container>
   );
 }
